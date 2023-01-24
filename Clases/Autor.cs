6a431bd9a0c60e25c8bcc647c12637cab9f4eee1
JuanMateoSanchez <90640548+JuanMateoSanchez@users.noreply.github.com>
2023-01-24 15:02:07 +0100

    Clases Autor y Articulo Empezadas Juan

---
+++ Clases/Autor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRevista.Clases
+{
+    class Autor : ObservableObject
+    {
+        private string nombre;
+        private string nickname;
+        private string image;
+        private ObservableCollection<Articulo> articulos;
+        private ObservableCollection<Articulo> articulosPublicados;
+        private ObservableCollection<Articulo> articulosSinPublicar;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { SetProperty(ref nombre, value); }
+        }
+    }
+}
