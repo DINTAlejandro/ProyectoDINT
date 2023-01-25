513aeda6a6d9246feb4205dcb7240193c9dfd481
alumno <alumno@A302PC22>
2023-01-25 12:44:11 +0100

    Creacion de las tablas

--- Servicios/ServicioBD.cs
+++ Servicios/ServicioBD.cs
@@ -16,11 +16,24 @@ namespace ProyectoRevista
             conexion.Open();
 
             SqliteCommand comando = conexion.CreateCommand();
-            comando.CommandText = @"CREATE TABLE IF NOT EXISTS Autores (nombre varchar(20) primary key, nickname varchar(20), image varchar(20), imageRed varchar(20))";
+            comando.CommandText = @"CREATE TABLE IF NOT EXISTS Autores 
+                                    (nombre varchar(20) primary key, nickname varchar(20) DEFAULT NULL, 
+                                     image varchar(20) DEFAULT NULL, imageRed varchar(20)) DEFAULT NULL";
 
             comando.ExecuteNonQuery();
 
+            comando.CommandText = @"CREATE TABLE IF NOT EXISTS Articulos 
+                                    (titulo varchar(20) primary key, imagen varchar(100) DEFAULT NULL, 
+                                     texto varchar(200) DEFAULT NULL, seccion varchar(20) DEFAULT NULL,
+                                     autor varchar(20), publicado BIT DEFAULT NULL, 
+                                     FOREIGN KEY (autor) REFERENCES Autores(nombre), FOREIGN KEY (seccion) REFERENCES Secciones(name))";
 
+            comando.ExecuteNonQuery();
+
+            comando.CommandText = @"CREATE TABLE IF NOT EXISTS Secciones 
+                                    (name varchar(20) primary key)"; 
+
+            comando.ExecuteNonQuery();
         }
     }
 }
