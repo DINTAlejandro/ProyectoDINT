ea1d6b780c51466a25da07f8a28b6956933a11c0
dam1alejandro <90453213+dam1alejandro@users.noreply.github.com>
2023-01-25 12:53:18 +0100

    Editar Autor

---
+++ VistasModelo/EditarAutorVM.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using ProyectoRevista.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRevista.VistasModelo
+{
+    class EditarAutorVM : ObservableRecipient
+    {
+        private Autor autorActual;
+        public Autor AutorActual
+        {
+            get { return autorActual; }
+            set { SetProperty(ref autorActual, value); }
+        }
+
+        public EditarAutorVM()
+        {
+
+        }
+    }
+}
